29856f43301b022f389b908c847b593d15bea0ff
Peter Lehnhardt <e0017@outlook.com>
2023-06-12 19:52:16 +0200

    Massive new commit with new version

---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+import {z, defineCollection, reference} from "astro:content";
+
+const project = defineCollection({
+    type: "content",
+    schema: z.object({
+        name: z.string(),
+        summary: z.string(),
+        techs: z.array(reference("tech")),
+    })
+});
+
+const tech = defineCollection({
+    type: "data",
+    schema: z.object({
+        name: z.string(),
+        description: z.string(),
+        link: z.string().url()
+    })
+});
+
+const job = defineCollection({
+    type: "data",
+    schema: z.object({
+        company: z.string(),
+        jobDescription: z.string(),
+        startDate: z.string().datetime(),
+        endDate: z.nullable(z.string().datetime()),
+        tasks: z.array(z.string()),
+        techs: z.array(reference("tech"))
+    })
+});
+
+export const collections = {project, tech, job};
