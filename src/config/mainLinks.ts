5dfa5c64a2e0b3839bfd7d62f20e31227be9102a
Peter Lehnhardt <e0017@outlook.com>
2023-06-21 20:55:44 +0200

    Add technologies page

--- src/config/mainLinks.ts
+++ src/config/mainLinks.ts
@@ -1,5 +1,8 @@
-export default [
+const mainLinks = [
     {text: "About Me", link: "/about"},
     {text: "Projects", link: "/projects"},
-    {text: "Career", link: "/career"}
-];
+    {text: "Career", link: "/career"},
+    {text: "Technologies", link: "/techs"}
+] as const;
+
+export default mainLinks;
